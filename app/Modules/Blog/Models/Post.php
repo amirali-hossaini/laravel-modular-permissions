b65ee59fbc3aed65bd1eeec1c720e3e86864f204
Amirali Hossaini <amirdev1024@gmail.com>
2026-01-03 02:25:44 +0330

    feat(blog): add Post model with user relationship and migration

---
+++ app/Modules/Blog/Models/Post.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Modules\Blog\Models;
+
+use App\Modules\User\Models\User;
+use Database\Factories\PostFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [
+        'id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected static function newFactory(): PostFactory
+    {
+        return PostFactory::new();
+    }
+}
