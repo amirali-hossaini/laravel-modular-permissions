c5a2f22976b95515fce8da6e27eaa7da9dc4a99d
Amirali Hossaini <amirdev1024@gmail.com>
2026-01-03 20:17:23 +0330

    feat: add RoleData DTO for role management

---
+++ app/Modules/AccessControl/Data/RoleData.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Modules\AccessControl\Data;
+
+use Spatie\LaravelData\Attributes\MapInputName;
+use Spatie\LaravelData\Attributes\MapOutputName;
+use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Mappers\SnakeCaseMapper;
+
+#[MapInputName(SnakeCaseMapper::class)]
+#[MapOutputName(SnakeCaseMapper::class)]
+class RoleData extends Data
+{
+    public function __construct(
+        public string $name,
+        public array $permissionIds,
+        public ?string $description = null,
+    ) {}
+}
