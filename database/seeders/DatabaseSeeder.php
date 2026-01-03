f15242341ca318823646ca27f4ce2abe7feed641
Amirali Hossaini <amirdev1024@gmail.com>
2026-01-03 20:21:12 +0330

    chore: seed admin user with all permissions and sample posts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,20 +2,36 @@
 
 namespace Database\Seeders;
 
+use App\Modules\AccessControl\Models\Permission;
+use App\Modules\AccessControl\Models\Role;
+use App\Modules\Blog\Models\Post;
+use App\Modules\User\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        $this->call([
-            PermissionSeeder::class,
+        $this->call(PermissionSeeder::class);
+
+        $user = User::factory()->create([
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
         ]);
+
+        $role = Role::factory()->create([
+            'name' => 'admin',
+        ]);
+
+        $role->permissions()->attach(Permission::pluck('id')->toArray());
+
+        $user->roles()->attach($role);
+
+        Post::factory()->count(5)->state(['user_id' => $user->id])->create();
+        Post::factory()->count(10)->create();
     }
 }
