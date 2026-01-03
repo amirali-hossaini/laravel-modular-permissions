42c55f47854562e269c17552063201edfd6983d7
Amirali Hossaini <amirdev1024@gmail.com>
2026-01-03 20:19:35 +0330

    feat: define AccessControl API routes

---
+++ app/Modules/AccessControl/Routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Modules\AccessControl\Http\Controllers\RoleController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1/access-control')->name('accessControl.')
+    ->middleware('auth:sanctum')
+    ->group(function () {
+        Route::apiResource('roles', RoleController::class);
+    });
