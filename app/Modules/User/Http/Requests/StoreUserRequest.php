4dbdc002bc995ca76591741eaad03fa6b3a4a4ff
Amirali Hossaini <amirdev1024@gmail.com>
2026-01-03 15:14:34 +0330

    feat: add validation requests for user creation and updates

---
+++ app/Modules/User/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Modules\User\Http\Requests;
+
+use App\Modules\AccessControl\Models\OrganizationalChart;
+use App\Modules\AccessControl\Models\Permission;
+use App\Modules\AccessControl\Models\Role;
+use App\Modules\User\Models\User;
+use Illuminate\Validation\Rule;
+use Omalizadeh\ApiResponse\Requests\BaseApiRequest;
+
+class StoreUserRequest extends BaseApiRequest
+{
+    protected $stopOnFirstFailure = true;
+
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+            ],
+            'email' => [
+                'required',
+                'string',
+                'email',
+                Rule::unique(User::class, 'email'),
+                'max:255',
+            ],
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                'max:255',
+            ],
+            'permission_ids' => [
+                'array',
+                'required_without_all:role_ids,organizational_chart_ids',
+            ],
+            'permission_ids.*' => [
+                'integer',
+                Rule::exists(Permission::class, 'id'),
+            ],
+            'role_ids' => [
+                'array',
+                'required_without_all:permission_ids,organizational_chart_ids',
+            ],
+            'role_ids.*' => [
+                'integer',
+                Rule::exists(Role::class, 'id'),
+            ],
+            'organizational_chart_ids' => [
+                'array',
+                'required_without_all:permission_ids,role_ids',
+            ],
+            'organizational_chart_ids.*' => [
+                'integer',
+                Rule::exists(OrganizationalChart::class, 'id'),
+            ],
+        ];
+    }
+}
