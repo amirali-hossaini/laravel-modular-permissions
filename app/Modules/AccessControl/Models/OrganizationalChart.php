3941f1cb9d68f341103cdc6f6a841d49ffcb5eee
Amirali Hossaini <amirdev1024@gmail.com>
2026-01-04 10:13:01 +0330

    feat: add factory support to OrganizationalChart model

--- app/Modules/AccessControl/Models/OrganizationalChart.php
+++ app/Modules/AccessControl/Models/OrganizationalChart.php
@@ -2,6 +2,7 @@
 
 namespace App\Modules\AccessControl\Models;
 
+use Database\Factories\OrganizationalChartFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -17,5 +18,10 @@ class OrganizationalChart extends Model
     public function permissions(): BelongsToMany
     {
         return $this->belongsToMany(Permission::class);
+    }
+
+    protected static function newFactory(): OrganizationalChartFactory
+    {
+        return OrganizationalChartFactory::new();
     }
 }
