788006e40e56918753b561f77485166d49192cf7
Amirali Hossaini <amirdev1024@gmail.com>
2026-01-04 11:51:25 +0330

    feat: enhance TestCase with authentication and permission helpers

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,33 @@
 
 namespace Tests;
 
+use App\Modules\AccessControl\Models\Permission;
+use App\Modules\User\Models\User;
+use Database\Seeders\PermissionSeeder;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Laravel\Sanctum\Sanctum;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected User $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed(PermissionSeeder::class);
+
+        $this->user = User::factory()->create();
+
+        Sanctum::actingAs($this->user);
+    }
+
+    protected function hasPermission(string $permissionName): void
+    {
+        $permission = Permission::firstOrCreate([
+            'name' => $permissionName,
+        ]);
+
+        $this->user->permissions()->attach($permission->id);
+    }
 }
