09a2a9851e45103af64ff492026246a09b24dd1b
Amirali Hossaini <amirdev1024@gmail.com>
2026-01-04 10:13:16 +0330

    feat: add organizational-charts API resource routes

--- app/Modules/AccessControl/Routes/api.php
+++ app/Modules/AccessControl/Routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Modules\AccessControl\Http\Controllers\OrganizationalChartController;
 use App\Modules\AccessControl\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,4 +8,5 @@ Route::prefix('v1/access-control')->name('accessControl.')
     ->middleware('auth:sanctum')
     ->group(function () {
         Route::apiResource('roles', RoleController::class);
+        Route::apiResource('organizational-charts', OrganizationalChartController::class);
     });
