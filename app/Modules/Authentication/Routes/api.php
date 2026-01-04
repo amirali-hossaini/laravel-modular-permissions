ebcbb078c5d797a4c121266e5bf9ae653e74a190
Amirali Hossaini <amirdev1024@gmail.com>
2026-01-04 12:11:07 +0330

    refactor: update authentication routes prefix and naming

--- app/Modules/Authentication/Routes/api.php
+++ app/Modules/Authentication/Routes/api.php
@@ -3,7 +3,7 @@
 use App\Modules\Authentication\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1/auth')->name('auth.')
+Route::prefix('v1/authentication')->name('authentication.')
     ->controller(AuthController::class)
     ->group(function () {
         Route::post('login', 'login')->name('login');
