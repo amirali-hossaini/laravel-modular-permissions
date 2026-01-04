69d6a8b1bced1c511fc730f0b55599e887f39da2
Amirali Hossaini <amirdev1024@gmail.com>
2026-01-04 10:13:26 +0330

    feat: register OrganizationalChartRepository in service provider

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Modules\AccessControl\Repositories\Interfaces\OrganizationalChartRepositoryInterface;
 use App\Modules\AccessControl\Repositories\Interfaces\RoleRepositoryInterface;
+use App\Modules\AccessControl\Repositories\OrganizationalChartRepository;
 use App\Modules\AccessControl\Repositories\RoleRepository;
 use App\Modules\Blog\Repositories\Interfaces\PostRepositoryInterface;
 use App\Modules\Blog\Repositories\PostRepository;
@@ -17,5 +19,6 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->singleton(UserRepositoryInterface::class, UserRepository::class);
         $this->app->singleton(PostRepositoryInterface::class, PostRepository::class);
         $this->app->singleton(RoleRepositoryInterface::class, RoleRepository::class);
+        $this->app->singleton(OrganizationalChartRepositoryInterface::class, OrganizationalChartRepository::class);
     }
 }
